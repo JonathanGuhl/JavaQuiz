3265f52748c9e18a2bbce83a67c30d44a82858a9
JonG <teflonjuan@icloud.com>
2023-01-08 03:31:17 -0600

    add HTML code and Quiz Questions to Java

---
+++ assets/script.js
@@ -0,0 +1,55 @@
+
+// Quiz Questions
+var quizQuestions = [{
+    question: "In how many ways a JavaScript code can be involved in an HTML file?",
+        a: "Inline",
+        b: "Internal",
+        c: "External",
+        d: "All of the Above",
+        correctAnswer: "d"},
+   {
+    question: "Which keyword exits a function?",
+        a: "var",
+        b: "return",
+        c: "switch",
+        d: "function",
+        correctAnswer: "b"},
+    {
+    question: "A JavaScript function is defined with the function keyword, followed by a name, followed by ___?",
+        a: "()",
+        b: "[]",
+        c: "{}",
+        d: "<>",
+        correctAnswer: "a"},
+    {
+    question: "JavaScript is case sensitive?",
+        a: "True",
+        b: "False",
+        correctAnswer: "a"},
+    {
+    question: "JavaScript has ___ Datatypes?",
+        a: "4",
+        b: "10",
+        c: "8",
+        d: "16",
+        correctAnswer: "c"},  
+    {
+    question: "Local variables can only be accessed from within the function?",
+        a: "True",
+        b: "False",
+        correctAnswer: "a"},
+    {
+    question: "Arrays are special types of _____?",
+        a: "methods",
+        b: "functions",
+        c: "events",
+        d: "objects",
+        correctAnswer: "d"},
+    {
+    questino: "Boolean has what data type?",
+        a: "String",
+        b: "Number",
+        c: "Undefined",
+        d: "True/False",
+        correctAnswer: "d"},
+    ];
